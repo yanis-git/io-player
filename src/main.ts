de94b549aaf93e415683be912e9d8f16cc445ebd
yanis <yanis@ipedis.com>
2022-03-28 10:28:05 +0400

    upgrade dependency - move packagr config to separate file

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * Section polyfill
  */
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
 // Used for browsers with partially native support of Custom Elements
 import '@webcomponents/custom-elements/src/native-shim';
 // Used for browsers without a native support of Custom Elements
